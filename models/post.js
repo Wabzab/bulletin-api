5c6db5628a298a03c79999464b59d155f8e4c30b
Max Paterson-Jones <100119585+Wabzab@users.noreply.github.com>
2023-09-28 08:45:11 +0200

    Revert "Initial Commit"
    This reverts commit 005e1aba4ac1435ff73594864346aae8c52cc67a.

--- models/post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const postschema = mongoose.Schema({
-    departments: {type: [String], required: true},
-    issue: {type: String, required: true}
-})
-
-module.exports = mongoose.model("Post", postschema);
